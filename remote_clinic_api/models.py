a2d8a47ae9589f917f8ecd04efe3c8b090afcc71
Bilal Bhatti <bilalwasi27pc@gmail.com>
2020-04-28 21:42:09 +0500

    MODIIFED: models.py Added: Doctor Schema and Document Schema

--- remote_clinic_api/models.py
+++ remote_clinic_api/models.py
@@ -1,5 +1,7 @@
 from remote_clinic_api import db
-from mongoengine import StringField, EmailField, DateTimeField, BooleanField, DictField
+from mongoengine import StringField, EmailField, DateTimeField, BooleanField, DictField, EmbeddedDocument, EmbeddedDocumentField, Document, FloatField, ReferenceField, ObjectIdField  
+
+
 from marshmallow_mongoengine import ModelSchema
 from datetime import datetime
 
@@ -19,3 +21,55 @@ class Patient(db.Document):
 class PatientSchema(ModelSchema):
     class Meta:
         model = Patient
+
+
+
+class Address(EmbeddedDocument):
+    country = StringField(),
+    city = StringField(),
+    street = StringField(),
+
+class Doctor(Document):
+    name = StringField()
+    surname = StringField()
+    email = EmailField()
+    phone = StringField()
+    gender = StringField()
+    address = EmbeddedDocumentField(Address)
+    image_path = StringField()
+    signup_date = DateTimeField(default=datetime.utcnow)
+    online = BooleanField()
+    specilization = StringField()
+    about = StringField()
+    experience = StringField()
+    pmdc_verified = BooleanField()
+    pmdc_reg_num = StringField()
+    verification_status = StringField()
+    verified_by = ObjectIdField()
+    verification_date = DateTimeField()
+
+# defining schema for json serialization
+class DoctorSchema(ModelSchema):
+    class Meta:
+        model = Doctor
+
+class DDocuments(Document):
+    owner: ObjectIdField()
+    title: StringField()
+    type_ = StringField()
+    description = StringField()
+    issued_by_org = StringField()
+    issued_date = DateTimeField()
+    img_path = StringField(),
+    verification_status = StringField(),
+    verified_by = ObjectIdField(),
+    verification_date = DateTimeField(),
+    rejection_cause = StringField()
+
+# defining schema for json serialization
+class DDcoumentsSchema(ModelSchema):
+    class Meta:
+        model = DDcoumentsSchema
+
+
+
