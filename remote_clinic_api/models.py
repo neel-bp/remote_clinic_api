9d37778ff28c197b68f34102503f6110b72fc596
neelu0 <neelu0@protonmail.com>
2020-04-28 20:56:35 +0500

    reviews new class

--- remote_clinic_api/models.py
+++ remote_clinic_api/models.py
@@ -1,5 +1,5 @@
 from remote_clinic_api import db
-from mongoengine import StringField, EmailField, DateTimeField, BooleanField, DictField
+from mongoengine import StringField, EmailField, DateTimeField, BooleanField, DictField, FloatField, ReferenceField
 from marshmallow_mongoengine import ModelSchema
 from datetime import datetime
 
@@ -13,9 +13,20 @@ class Patient(db.Document):
     image_path = StringField()
     signup_date = DateTimeField(default=datetime.utcnow)
     online = BooleanField()
-    reviews = DictField()
+
 
 # defining schema for json serialization
 class PatientSchema(ModelSchema):
     class Meta:
         model = Patient
+
+class Reviews(db.Document):
+    rating = FloatField(max_value=5)
+    review_by = ReferenceField('Patient')
+    # review_for = ReferenceField(Doctor) after Doctor class is created
+    review_date = DateTimeField(default=datetime.utcnow)
+    comment = StringField()
+
+class ReviewsSchema(ModelSchema):
+    class Meta:
+        model = Reviews
