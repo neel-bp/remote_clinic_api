dc392a521616273bd86eaeceed4898f0184d157c
neelu0 <neelu0@protonmail.com>
2020-04-29 17:08:20 +0500

    added all models related to patient and person, also handled get and post request to patients route, put and delete still remaining

--- remote_clinic_api/models.py
+++ remote_clinic_api/models.py
@@ -1,18 +1,31 @@
 from remote_clinic_api import db
-from mongoengine import StringField, EmailField, DateTimeField, BooleanField, DictField, FloatField, ReferenceField
+from mongoengine import StringField, EmailField, DateTimeField, BooleanField, DictField, FloatField, ReferenceField, EmbeddedDocument, EmbeddedDocumentField, EmbeddedDocumentListField
 from marshmallow_mongoengine import ModelSchema
 from datetime import datetime
 
+class Address(EmbeddedDocument):
+    latitude = FloatField()
+    longitude = FloatField()
+    country = StringField()
+    city = StringField()
+    street = StringField()
+
+class AddressSchema(ModelSchema):
+    class Meta:
+        model = Address
+
 class Patient(db.Document):
+    username = StringField()
+    password = StringField()
     name = StringField()
     surname = StringField()
     email = EmailField()
     phone = StringField()
     gender = StringField()
-    address = StringField()
     image_path = StringField()
     signup_date = DateTimeField(default=datetime.utcnow)
     online = BooleanField()
+    address = EmbeddedDocumentField(Address)
 
 
 # defining schema for json serialization
@@ -23,10 +36,60 @@ class PatientSchema(ModelSchema):
 class Reviews(db.Document):
     rating = FloatField(max_value=5)
     review_by = ReferenceField('Patient')
-    # review_for = ReferenceField(Doctor) after Doctor class is created
+    # review_for = ReferenceField('Doctor') after Doctor class is created
     review_date = DateTimeField(default=datetime.utcnow)
     comment = StringField()
 
 class ReviewsSchema(ModelSchema):
     class Meta:
         model = Reviews
+
+
+class Drug(EmbeddedDocument):
+    name = StringField()
+    dose = StringField()
+    time = StringField()
+    description = StringField()
+
+class DrugSchema(ModelSchema):
+    class Meta:
+        model = Drug
+
+## patient prescriptions
+class Prescription(db.Document):
+    prescribed_by = StringField()
+    #doctor_id = ReferenceField('Doctor') doctor id is used because if a user want to visit doctor page this id is used to find that doctor instead of doctor's name because name is aconflicting field multiple people can have same names
+    notes = StringField()
+    drugs = EmbeddedDocumentListField(Drug)
+    prescription_date = DateTimeField(datetime.utcnow)
+    prescribed_for = ReferenceField('Patient')
+
+class PrescriptionSchema(ModelSchema):
+    class Meta:
+        model = Prescription
+
+# patient interactions with doctors
+class Interaction(db.Document):
+    patient_id = ReferenceField('Patient')
+    #doctor_id = ReferenceField('Doctor')
+    doctor_name = StringField()
+    medium = StringField()
+    date = DateTimeField(default=datetime.utcnow)
+    interaction_type = StringField() # didnt use type because type is a reserved word in python
+
+class InteractionSchema(ModelSchema):
+    class Meta:
+        model = Interaction
+
+class Reports(db.Document):
+    title = StringField()
+    report_type = StringField()
+    description = StringField()
+    issued_by_org = StringField()
+    issued_date = DateTimeField(default=datetime.utcnow)
+    file_path = StringField()
+    report_of = ReferenceField('Patient')
+
+class ReportSchema(ModelSchema):
+    class Meta:
+        model = Reports
--- remote_clinic_api/routes.py
+++ remote_clinic_api/routes.py
@@ -1,7 +1,32 @@
 from remote_clinic_api import app, db
-from flask import jsonify
+from flask import jsonify, request
+from remote_clinic_api.models import *
 
 
 @app.route('/')
 def hello():
     return jsonify({'hello':'world'})
+
+@app.route('/patients', methods = ['GET','POST','PUT','DELETE'])
+def patients():
+    if request.method == 'GET':    
+        patient_objects = Patient.objects.exclude('password')
+        patient_schema = PatientSchema()
+        p_list_json = []
+        if request.args.get('limit') is not None:
+            limit = request.args.get('limit')
+            for i in patient_objects[:int(limit)]:
+                p_list_json.append(patient_schema.dump(i))
+        else:
+            for j in patient_objects:
+                p_list_json.append(patient_schema.dump(j))
+        return jsonify(p_list_json)
+    
+    elif request.method == 'POST':
+        patient_json = request.json
+        patient_schema = PatientSchema()
+        patient = patient_schema.load(patient_json)
+        patient.save()
+        new_patient = patient_schema.dump(patient)
+        return jsonify(new_patient)
+
