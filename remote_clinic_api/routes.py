190e53be55488bb88cd650a572c78d1f27d67bc6
neelu0 <neelu0@protonmail.com>
2020-04-30 15:55:20 +0500

    added new route for getting and updating specific patient

--- remote_clinic_api/routes.py
+++ remote_clinic_api/routes.py
@@ -1,13 +1,14 @@
 from remote_clinic_api import app, db
 from flask import jsonify, request
 from remote_clinic_api.models import *
+import json
 
 
 @app.route('/')
 def hello():
     return jsonify({'hello':'world'})
 
-@app.route('/patients', methods = ['GET','POST','PUT','DELETE'])
+@app.route('/patients', methods = ['GET','POST'])
 def patients():
     if request.method == 'GET':    
         patient_objects = Patient.objects.exclude('password')
@@ -30,3 +31,17 @@ def patients():
         new_patient = patient_schema.dump(patient)
         return jsonify(new_patient)
 
+@app.route('/patients/<string:patient_id>', methods=['GET','PATCH','PUT'])
+def get_patient(patient_id):
+    if request.method == 'GET':
+        patient_object = Patient.objects.exclude('password').get_or_404(id=str(patient_id))
+        patient_schema = PatientSchema()
+        return jsonify(patient_schema.dump(patient_object))
+
+    elif request.method == 'PATCH' or request.method == 'PUT':
+        updated_fields = request.json
+        patient_dic = PatientSchema().dump(Patient.objects.get_or_404(id=str(patient_id)))
+        patient_dic.update(updated_fields)
+        updated_patient = PatientSchema().load(patient_dic)
+        updated_patient.save()
+        return jsonify(PatientSchema().dump(updated_patient))
