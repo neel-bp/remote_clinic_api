833938f4ed3a5ef221b0588457f58532051bd7a7
neelu0 <neelu0@protonmail.com>
2020-05-01 15:48:33 +0500

    added back new models

--- remote_clinic_api/models.py
+++ remote_clinic_api/models.py
@@ -164,3 +164,35 @@ class Reviews(db.Document):
 class ReviewsSchema(ModelSchema):
     class Meta:
         model = Reviews
+
+class Permissions(EmbeddedDocument):
+    title = StringField(required=True)
+
+# defining schema for json serialization
+class PermisstionSchema(ModelSchema):
+    class Meta:
+        model = Permissions
+
+
+# Note: One Role can have multiple permissions: one to many relationship
+# Note: When creating new Role permissions should be already defined.
+class Roles(db.Document):
+    title = StringField(required=True)
+    permissions = EmbeddedDocumentListField(Permissions, required=True) # List of permissions
+
+
+# defining schema for json serialization
+class RolesSchema(ModelSchema):
+    class Meta:
+        model = Roles
+
+# Note: One Operator have one Role - one to one relationship.
+class OperatorRoles(db.Document):
+    operator = ReferenceField('Operator', required=True)
+    role = ReferenceField('Roles', required=True)
+
+
+# defining schema for json serialization
+class OperatorRolesSchema(ModelSchema):
+    class Meta:
+        model = OperatorRoles
